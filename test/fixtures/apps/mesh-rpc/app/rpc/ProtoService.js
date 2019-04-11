339e81ca9afeec52b25c62d7642757cf8c4748b0
zhoukk <izhoukk@gmail.com>
2019-04-11 15:09:08 +0800

    fix test

--- test/fixtures/apps/mesh-rpc/app/rpc/ProtoService.js
+++ test/fixtures/apps/mesh-rpc/app/rpc/ProtoService.js
@@ -1,7 +1,6 @@
 'use strict';
 
 exports.echoObj = async function(req) {
-  req = req.toObject({ enums: String });
   return {
     code: 200,
     message: 'hello ' + req.name + ', you are in ' + req.group,
